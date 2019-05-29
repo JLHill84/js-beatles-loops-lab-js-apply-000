812f8f2d9b927af27a2a00976e59e631de0d7a2e
Joshua Hill <josh@jlhworkshop.com>
2019-05-29 03:35:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // add solution here
 var musicians=Array;
+musicians.push("John Lennon","Paul McCartney","George Harrison","Ringo Starr");
 var instruments=Array;
-for()
-function theBeatlesPlay(x,y)
+var empty=Array;
+for(empty)
+function theBeatlesPlay(musicians,instruments) {
+  
+}
