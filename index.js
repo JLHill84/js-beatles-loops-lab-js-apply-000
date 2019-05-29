f5825377bc2365f3e886a5d2a11b7826f16b7e7a
Joshua Hill <josh@jlhworkshop.com>
2019-05-29 03:41:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ musicians.push("John Lennon","Paul McCartney","George Harrison","Ringo Starr");
 var instruments=Array;
 instruments.push(" plays guitar"," sings"," plays bass"," plays drums")
 var empty=Array;
-for(empty)
 function theBeatlesPlay(musicians,instruments) {
-  
+  var arrayCount = musicians.list;
+  for(arrayCount){
+  empty.push(musicians[arrayCount] + instruments[arrayCount])
 }
